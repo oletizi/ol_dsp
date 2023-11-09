1e765084b6c0a5b6804576dc28a97cc0fc2a9875
orion <oletizi@mac.com>
2023-11-08 16:51:49 -0800

    - added Voice to realtime JUCE test

--- MyCallback.h
+++ MyCallback.h
@@ -7,14 +7,15 @@
 
 #include <daisysp.h>
 #include <juce_audio_devices/juce_audio_devices.h>
+#include "Voice.h"
 
 class MyCallback : public juce::AudioIODeviceCallback {
 private:
     uint32_t counter_ = 0;
     juce::AudioDeviceManager * device_manager_;
-    daisysp::Oscillator osc_;
 public:
-    explicit MyCallback(juce::AudioDeviceManager *device_manager) : device_manager_(device_manager){
+    explicit MyCallback(juce::AudioDeviceManager *device_manager, ol::synthlib::Voice *pVoice)
+            : device_manager_(device_manager), voice_(pVoice){
         std::cout << "MyCallback ctor" << std::endl;
         device_manager->addAudioCallback(this);
     }
@@ -31,7 +32,7 @@ public:
                                           const juce::AudioIODeviceCallbackContext &context) override {
         counter_++;
         for (int i = 0; i < numSamples; i++) {
-            float value = osc_.Process();
+            float value = voice_->Process();
             for (int j =0; j<numOutputChannels; j++) {
                 outputChannelData[j][i] = value;
             }
@@ -40,10 +41,7 @@ public:
 
     void audioDeviceAboutToStart(juce::AudioIODevice *device) override {
         std::cout << "Device about to start..." << std::endl;
-        osc_.Init(static_cast<float>(device->getCurrentSampleRate()));
-        osc_.SetFreq(440);
-        osc_.SetAmp(1);
-        osc_.SetWaveform(daisysp::Oscillator::WAVE_POLYBLEP_SAW);
+        voice_->Init(static_cast<float>(device->getCurrentSampleRate()));
     }
 
     void audioDeviceStopped() override {
@@ -51,7 +49,7 @@ public:
     }
 
 private:
-
+    ol::synthlib::Voice *voice_;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,13 +1,22 @@
 #include <iostream>
 #include "MyCallback.h"
+#include <ol_synthlib.h>
+
+using namespace ol::synthlib;
 
 int main() {
 
     juce::initialiseJuce_GUI();
     juce::AudioDeviceManager deviceManager = juce::AudioDeviceManager();
     deviceManager.initialiseWithDefaultDevices(2, 2);
-    MyCallback callback = MyCallback(&deviceManager);
+    t_sample frequency = 440;
+    Voice voice = Voice();
+    voice.SetFrequency(frequency);
+    MyCallback callback = MyCallback(&deviceManager, &voice);
+
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "u: increase frequency" << std::endl;
+    std::cout << "d: decrease frequency" << std::endl;
     std::cout << "t: play test sound" << std::endl;
     std::cout << "q: quit" << std::endl;
     while (auto c = getchar()) {
@@ -16,6 +25,16 @@ int main() {
         }
         if (c == 'q' || c == 'Q') {
             break;
+        }
+        if (c == 'u') {
+            frequency *= 2;
+            voice.SetFrequency(frequency);
+            std::cout << "New frequency: " << frequency << std::endl;
+        }
+        if (c == 'd') {
+            frequency /= 2;
+            voice.SetFrequency(frequency);
+            std::cout << "New frequency: " << frequency << std::endl;
         }
     }
     std::cout << "Goodbye!" << std::endl;
