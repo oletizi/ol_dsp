329a9a7c4447e3fc5e917e31ebdf630faf3e5b3f
orion <oletizi@mac.com>
2023-11-11 14:44:34 -0800

    - added second delay line for stereo

--- fxlib/FxChain.cpp
+++ fxlib/FxChain.cpp
@@ -7,15 +7,15 @@
 int ol::fxlib::FxChain::Process(const float &in1, const float &in2, float *out1, float *out2) {
     float wet_in_1 = 0, wet_in_2 = 0, wet_out_1 = 0, wet_out_2 = 0;
     int rv = 0;
-    delay1_.UpdateDelayTime(delay_time_);
-    delay1_.UpdateResonance(delay_resonance_);
-    delay1_.UpdateCutoff(delay_cutoff_);
-    delay1_.UpdateResonance(delay_resonance_);
+    updateDelays();
     *out1 = 0;
     *out2 = 0;
 
-    wet_in_1 += (delay1_.Process(in1) * delay_balance_) + (in1 * (1 - delay_balance_));
-    wet_in_2 += in2;
+    // process delay lines
+    wet_in_1 += (delay_1_.Process(in1) * delay_balance_) + (in1 * (1 - delay_balance_));
+    wet_in_2 += (delay_2_.Process(in2) * delay_balance_) + (in1 * (1 - delay_balance_));
+
+    // process reverb
     rv += verb_.Process(wet_in_1, wet_in_2, &wet_out_1, &wet_out_2);
     *out1 += (wet_out_1 * verb_balance_) + (wet_in_1 * (1 - verb_balance_));
     *out2 += (wet_out_2 * verb_balance_) + (wet_in_2 * (1 - verb_balance_));
--- fxlib/FxChain.h
+++ fxlib/FxChain.h
@@ -16,21 +16,35 @@ namespace ol::fxlib {
             verb_.SetFeedback(verb_feedback_);
             verb_.SetLpFreq(verb_cutoff_);
 
-            delay1_.Init(sample_rate);
-            delay1_.UpdateDelayTime(delay_time_);
-            delay1_.UpdateFeedback(delay_feedback_);
-            delay1_.UpdateCutoff(delay_cutoff_);
+            delay_1_.Init(sample_rate);
+            delay_2_.Init(sample_rate);
+            updateDelays();
         }
 
         int Process(const sample_t &in1, const sample_t &in2, sample_t *out1, sample_t *out2);
 
     private:
+
+        /**
+         * Update parameters on the delay lines.
+         */
+        void updateDelays() {
+            delay_1_.UpdateDelayTime(delay_time_);
+            delay_2_.UpdateDelayTime(delay_time_);
+
+            delay_1_.UpdateFeedback(delay_feedback_);
+            delay_2_.UpdateFeedback(delay_feedback_);
+
+            delay_1_.UpdateCutoff(delay_cutoff_);
+            delay_2_.UpdateCutoff(delay_cutoff_);
+        }
         daisysp::ReverbSc verb_;
         sample_t verb_feedback_ = 0.75;
         sample_t verb_cutoff_ = 5000;
         sample_t verb_balance_ = 0.5;
 
-        Delay delay1_;
+        Delay delay_1_;
+        Delay delay_2_;
         sample_t delay_time_ = 30000;
         sample_t delay_feedback_ = 0.3;
         sample_t delay_cutoff_ = 5000;
