806a88d5ec1fea808cc8fb7d3ea3d7a9d474fd13
orion <oletizi@mac.com>
2023-11-12 14:07:41 -0800

    - passing audio file data to the fx chain seems to work.

--- workouts/SpyAudioSource.cpp
+++ workouts/SpyAudioSource.cpp
@@ -3,7 +3,9 @@
 //
 #include "SpyAudioSource.h"
 
-SpyAudioSource::SpyAudioSource(ol::fx::FxChain *fx, juce::AudioFormatReaderSource *source) : fx_(fx), source_(source), counter_(0), processed_(0) {}
+SpyAudioSource::SpyAudioSource(ol::fx::FxChain *fx, juce::AudioFormatReaderSource *source) : fx_(fx), source_(source),
+                                                                                             counter_(0),
+                                                                                             processed_(0) {}
 
 void SpyAudioSource::prepareToPlay(int samplesPerBlockExpected, double sampleRate) {
     source_->prepareToPlay(samplesPerBlockExpected, sampleRate);
@@ -21,13 +23,19 @@ void SpyAudioSource::getNextAudioBlock(const juce::AudioSourceChannelInfo &buffe
         counter_ = 0;
         processed_ = 0;
     }
-    juce::AudioBuffer<float> *buf = bufferToFill.buffer;
+    //const juce::AudioBuffer<float> *buf = bufferToFill.buffer;
+    const int start_sample = bufferToFill.startSample;
 
-    for (int i=0; i<bufferToFill.numSamples; i++) {
-
+    for (int i = start_sample; i < bufferToFill.numSamples; i++) {
+        float in1 = bufferToFill.buffer->getSample(0, i);
+        float in2 = bufferToFill.buffer->getSample(1, i);
+        float *out1 = bufferToFill.buffer->getWritePointer(0, i);
+        float *out2 = bufferToFill.buffer->getWritePointer(1, i);
+        fx_->Process(in1, in2, out1, out2);
     }
     if (counter_ % 100 == 0) {
-        std::cout << "Buffer size: " << bufferToFill.numSamples << std::endl;
+        std::cout << "Buffer size        : " << bufferToFill.numSamples << std::endl;
+        std::cout << "  start sample     : " << start_sample << std::endl;
         std::cout << "  Buffers processed: " << counter_ << std::endl;
         std::cout << "  Samples processed: " << processed_ << std::endl;
         std::cout << "  Total length     : " << source_->getTotalLength() << std::endl;
