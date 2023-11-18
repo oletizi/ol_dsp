5f20a19fabd4711ef95ebef1c89521e5cb714bfa
orion <oletizi@mac.com>
2023-11-18 12:25:18 -0800

    - fixed arithemetic precedence error calculating delay balance.

--- modules/fxlib/Delay.cpp
+++ modules/fxlib/Delay.cpp
@@ -16,7 +16,7 @@ t_sample ol::fx::Delay::Process(t_sample in) {
         feedback_ = cp_->feedback.Value();
     }
     if (cutoff_ != cp_->cutoff.Value()) {
-        cutoff_ = cp_ ->cutoff.Value();
+        cutoff_ = cp_->cutoff.Value();
         filt_svf_.SetFreq(cutoff_);
     }
     if (resonance_ != cp_->resonance.Value()) {
@@ -27,11 +27,11 @@ t_sample ol::fx::Delay::Process(t_sample in) {
         balance_ = cp_->balance.Value();
     }
     // assign current delay value to output
-    out = (delay_line_->Read() * balance_) + (in * 1-balance_) ;
+    out = (delay_line_->Read() * balance_) + (in * (1 - balance_));
 
     // prepare feedback
     filt_svf_.Process(out);
-    t_sample delay_input = (feedback_* filt_svf_.Low()) + in;
+    t_sample delay_input = (feedback_ * filt_svf_.Low()) + in;
 
     // write feedback into delay
     delay_line_->Write(delay_input);
