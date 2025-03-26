1df7e765404f56f3acb6330bce7a5eb7a47e5f62
orion <oletizi@mac.com>
2025-03-25 23:50:06 -0700

    * Adding support for individual JUCE apps

---
+++ modules/juce/hello/README.md
@@ -0,0 +1,4 @@
+# JUCE fragments to build up to a simple cli plugin host
+## References:
+* https://forum.juce.com/t/how-to-create-a-simple-plug-in-host/54642/3
+* https://forum.juce.com/t/hosting-plugins/44560
--- modules/juce/hello/juce_hello.cpp
+++ modules/juce/hello/juce_hello.cpp
@@ -3,7 +3,28 @@
 //
 
 #include <iostream>
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
-}
+#include <juce_gui_basics/juce_gui_basics.h>
+class JuceHello : public juce::JUCEApplication {
+public:
+    const juce::String getApplicationName() override {
+        return "JuceHello";
+    };
+
+    const juce::String getApplicationVersion() override {
+        return "0.1";
+    };
+
+    void initialise(const juce::String &commandLineParameters) override {
+        std::cout << "Initialising JuceHello..." << std::endl;
+    };
+
+    void shutdown() override {
+        std::cout << "Shutdown JuceHello..." << std::endl;
+    };
+};
+// #include <juce_audio_processors/juce_audio_processors.h>
+// int main() {
+//     std::cout << "Hello, World!" << std::endl;
+//     return 0;
+// }
+START_JUCE_APPLICATION(JuceHello)
