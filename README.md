d486b57c2f86da89c666a957337e868a83606219
orion <oletizi@mac.com>
2025-03-25 22:49:38 -0700

    * Adding support for individual JUCE apps

---
+++ README.md
@@ -0,0 +1,6 @@
+# ol_dsp: A big ball of experimental DSP goo 
+
+I am not a c/c++ programmer.
+
+REVIEW:
+* https://github.com/cpm-cmake/CPM.cmake
---
+++ modules/juce/hello/juce_hello.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Orion Letizi on 3/25/25.
+//
+
+#include <iostream>
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
