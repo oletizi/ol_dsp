9528a2edfe4d6dd19be325c3e908892614af7cd7
orion <oletizi@mac.com>
2025-03-27 13:24:07 -0700

    JUCE-based plugin host * instantiating plugins seems to work

--- modules/juce/host/host.cpp
+++ modules/juce/host/host.cpp
@@ -16,17 +16,16 @@ void OLJuceHost::initialise(const juce::String &commandLineParameters) {
     std::cout << "Initialising OLJuceHost..." << std::endl;
     formatManager.addDefaultFormats();
     std::cout << "Num formats: " << formatManager.getNumFormats() << std::endl;
-    const juce::String toLoad = "TAL"; // TODO: make this configurable
+    const juce::String toLoad = "TAL"; //TAL Reverb 4 Plugin"; // TODO: make this configurable
     for (int i = 0; i < formatManager.getNumFormats(); ++i) {
         constexpr int scanMax = 10000; // TODO: make this configurable
         constexpr bool recursive = true;
         juce::FileSearchPath path;
         juce::File deadMansPedalFile("~/tmp/deadPedals");
         juce::AudioPluginFormat *format = formatManager.getFormat(i);
-        juce::KnownPluginList list;
         juce::String pluginName;
         juce::PluginDirectoryScanner *scanner = new juce::PluginDirectoryScanner(
-            list, *format, path, recursive, deadMansPedalFile);
+            this->knownPlugins, *format, path, recursive, deadMansPedalFile);
 
         std::cout << "Scanning " << format->getName() << std::endl;
         bool more = true;
@@ -41,6 +40,22 @@ void OLJuceHost::initialise(const juce::String &commandLineParameters) {
                 scanner->skipNextFile();
             }
         }
+    }
+    // Add plugin
+    // formatManager.createPluginInstanceAsync (desc.pluginDescription,
+    //                                      graph.getSampleRate(),
+    //                                      graph.getBlockSize(),
+    //                                      [this, pos, dpiDisabler, useARA = desc.useARA] (std::unique_ptr<AudioPluginInstance> instance, const String& error)
+    //                                      {
+    //                                          addPluginCallback (std::move (instance), error, pos, useARA);
+    //                                      });
+    const juce::Array<juce::PluginDescription> plugs = this->knownPlugins.getTypes();
+    std::cout << "plugs count: " << plugs.size() << std::endl;
+    for (int i = 0; i < plugs.size(); ++i) {
+        const juce::PluginDescription &plugDescription = plugs[i];
+        juce::String errorMessage("barf.");
+        std::cout << "instantiating: " << plugDescription.name << std::endl;
+        std::unique_ptr<juce::AudioPluginInstance> plug = formatManager.createPluginInstance(plugDescription, 441000, 128, errorMessage);
     }
 }
 
