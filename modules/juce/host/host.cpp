2a4c4bcf7deea7ff423e3d2d406ffea77d0d6816
orion <oletizi@mac.com>
2025-03-26 19:42:26 -0700

    JUCE-based plugin host * JUCE plugin scanning sort of works

--- modules/juce/host/host.cpp
+++ modules/juce/host/host.cpp
@@ -3,18 +3,36 @@
 //
 
 #include "host.h"
-const juce::String OLJuceHost::getApplicationName()  {
-        return "JuceHello";
+
+const juce::String OLJuceHost::getApplicationName() {
+    return "JuceHello";
 }
 
 const juce::String OLJuceHost::getApplicationVersion() {
     return "0.1";
 }
 
-void OLJuceHost::initialise(const juce::String &commandLineParameters)  {
+void OLJuceHost::initialise(const juce::String &commandLineParameters) {
     std::cout << "Initialising OLJuceHost..." << std::endl;
+    formatManager.addDefaultFormats();
+    std::cout << "Num formats: " << formatManager.getNumFormats() << std::endl;
+    for (int i = 0; i < formatManager.getNumFormats(); ++i) {
+        constexpr bool recursive = true;
+        juce::FileSearchPath path;
+        juce::File deadMansPedalFile("~/tmp/deadPedals");
+        juce::AudioPluginFormat *format = formatManager.getFormat(i);
+        juce::KnownPluginList list;
+        juce::String pluginName;
+        juce::PluginDirectoryScanner *scanner = new juce::PluginDirectoryScanner(
+            list, *format, path, recursive, deadMansPedalFile);
+
+        std::cout << "Scanning " << format->getName() << std::endl;
+        while (scanner->scanNextFile(true, pluginName)) {
+            std::cout << "  Scanned: " << pluginName << std::endl;
+        }
+    }
 }
 
-void OLJuceHost::shutdown()  {
+void OLJuceHost::shutdown() {
     std::cout << "Shutdown OLJuceHost..." << std::endl;
 }
--- modules/juce/host/host.h
+++ modules/juce/host/host.h
@@ -4,6 +4,7 @@
 #pragma once
 #include <iostream>
 #include <juce_gui_basics/juce_gui_basics.h>
+#include <juce_audio_processors/juce_audio_processors.h>
 
 #ifndef HOST_H
 #define HOST_H
@@ -16,6 +17,10 @@ public:
     void initialise(const juce::String &commandLineParameters) override;
 
     void shutdown() override;
+
+private:
+    juce::AudioPluginFormatManager formatManager;
+    juce::KnownPluginList knownPlugins;
 };
 #endif //HOST_H
 START_JUCE_APPLICATION(OLJuceHost)
